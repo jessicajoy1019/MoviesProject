3149367665cd5af1ca804577c0fc57b9dea25cde
jessicajoy1019 <77498070+jessicajoy1019@users.noreply.github.com>
2021-01-31 19:47:49 -0500

    Update server.R
    final version

--- server.R
+++ server.R
@@ -2,34 +2,20 @@ library(DT)
 library(shiny)
 library(googleVis)
 library(tidyverse)
+library(dplyr)
 
 shinyServer(function(input, output){
   
   
-  output$intro <- renderText({ input$caption })
-  
-  
     # Map
-    
-    country_of_movies <- reactive ({
-        countrymovies %>% 
-            filter(get(input$Cselected)==1) %>% 
-            summarise(N=sum(Netflix=='1'),
-                      H=sum(Hulu=='1'),
-                      P=sum(`Prime Video`=='1'),
-                      D=sum(`Disney+`=='1')) 
-        })
-    
     output$map <- renderGvis({
-        gvisGeoChart(country_of_movies(),
-                     options=list(region='world', dataMode="markers", 
-                                  width="700", height="700"))
+        gvisGeoChart(cc, locationvar='country', colorvar=input$Cselected,
+                     options=list(region='world',backgroundColor='grey', 
+                                  colorAxis="{minValue: 0,  colors: ['#EFF5FB', '#0174DF']}",
+                                  datalessRegionColor="#FFFFFF", dataMode="markers", 
+                                  width="900", height="700"))
     })
-    # DataTable
-    output$table <- DT::renderDataTable({
-        datatable(movies, rownames=FALSE) 
-    })
-    
+
     #Age Distribution
     movie_age_data <- reactive ({
         
@@ -47,20 +33,38 @@ shinyServer(function(input, output){
             ggplot(aes(x="", y=n, fill=Age)) + 
             geom_bar(stat='identity') + 
             coord_polar("y", start=0) +
-            theme_void() + ggtitle(paste(input$selected, 'Age Distribution')) +
-            theme(plot.title = element_text(hjust = 0.5, size=22))
+            theme_void() + ggtitle(paste(input$selected, 'Age-Based Rating Distribution')) +
+            theme(plot.title = element_text(hjust = 0.5,face='bold', size=22),
+                  legend.title = element_text(hjust = 0.5, face="bold", size = 12)) +
+        geom_text(aes(label = paste(round(n / sum(n) * 100, 1), "%"), x = 1.6),
+                  position = position_stack(vjust = 0.5)) + 
+        scale_fill_manual(values = c("#FE2E2E","#FA8258","#F4FA58", "#81F781","#58ACFA"))
         
     })
     #Rating plots
     output$rating <- renderPlot({
-        m %>% ggplot(aes(x=`Rotten Tomatoes Score`, color=`Streaming Service`)) + 
-        geom_density(alpha = 1,size=1.3) + ggtitle('Rotten Tomatoes Rating') +
-        theme(plot.title = element_text(hjust = 0.5))
+        m %>% ggplot() +
+        geom_density(aes(x=`Rotten Tomatoes Score`, color=`Streaming Service`), alpha = 1,size=1.3) + 
+        ggtitle('Rotten Tomatoes Rating') +
+        theme(plot.title = element_text(hjust = 0.5, face="bold"),
+              panel.background = element_blank(),
+              panel.grid.major = element_line(colour='lightgrey'), 
+              panel.grid.minor = element_line(colour='lightgrey'),
+              legend.title = element_text(hjust = 0.5, face="bold", size = 12)) + 
+        scale_color_manual(values = c("red","lightgreen","royalblue1", "navyblue")) +
+        annotate("rect", xmin = 0, xmax = 60, ymin = 0, ymax = Inf,fill='greenyellow', alpha = 0.17) +
+        annotate("rect", xmin = 60, xmax = 100, ymin = 0, ymax = Inf,fill='firebrick1', alpha = 0.17) +
+        labs(caption="0% - 60%: Green Splat
+             60% - 100% : Red Tomato Score")
+
     })
     output$imdb <- renderPlot({
         IMDB_ %>% ggplot(aes(x=`IMDb Score`, color=`Streaming Service`)) + 
         geom_density(alpha = 1, size=1.3) + ggtitle('IMDb Rating') +
-        theme(plot.title = element_text(hjust = 0.5)) + xlim(0,10)
+        theme(plot.title = element_text(hjust = 0.5, face="bold"),
+              legend.title = element_text(hjust = 0.5, face="bold", size = 12)) + 
+        xlim(0,10) +
+        scale_color_manual(values = c("red","lightgreen","royalblue1", "navyblue")) 
       
     })
     
@@ -69,17 +73,33 @@ shinyServer(function(input, output){
       genremovies %>% 
         filter(get(input$Gselected)==1) %>%
         summarise(Netflix=sum(Netflix=='1'),
-                Hulu=sum(Hulu=='1'),
-                `Prime Video`=sum(`Prime Video`=='1'),
-                `Disney+`=sum(`Disney+`=='1')) %>%
-        gather( "Streaming Service", "Count") 
+                  Hulu=sum(Hulu=='1'),
+                  `Prime Video`=sum(`Prime Video`=='1'),
+                  `Disney+`=sum(`Disney+`=='1'))%>%
+        gather( "Streaming Service", "Count") %>% 
+        left_join(totals, by = 'Streaming Service') %>% 
+        mutate(Percent = round((Count/Total)*100, 2))
     })    
       output$genre <- renderPlot({
         movie_genre_data() %>% 
-        ggplot(aes(`Streaming Service`, Count)) + 
-        geom_bar(stat='identity')
+        ggplot(aes(`Streaming Service`, Percent, fill = `Streaming Service`)) + 
+        geom_bar(stat='identity') +
+          scale_fill_manual(values = c("navyblue",
+                                       "lightgreen",
+                                       "red", "royalblue1")) +
+          theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                panel.background = element_blank()) + 
+          ggtitle(paste(input$Gselected, "Movies")) +
+          theme(plot.title = element_text(hjust = 0.5, face="bold",  size=22),
+                legend.title = element_text(hjust = 0.5, face="bold", size = 12))
         
       })     
+      
+      # DataTable
+      output$table <- DT::renderDataTable({
+        datatable(movies, rownames=FALSE) 
+      })
+      
 
     
     })
