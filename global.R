e604bd110b8cd36541204237508344fe11d1d2ea
jessicajoy1019 <77498070+jessicajoy1019@users.noreply.github.com>
2021-01-31 19:44:01 -0500

    Update global.R
    final version

--- global.R
+++ global.R
@@ -1,7 +1,8 @@
-
+library(dplyr)
+library(data.table)
 movies <- read.csv(file = "movies.csv")
 movies <- movies %>% select(-Type, -X) %>%
-  rename(`Disney+`=Disney.,
+rename(`Disney+`=Disney.,
          `Prime Video`=Prime.Video,
          `Rotten Tomatoes`=Rotten.Tomatoes) %>%
   mutate(Country = as.character(Country)) %>% 
@@ -12,6 +13,7 @@ movies <- movies %>% select(-Type, -X) %>%
                            "18+", "16+", "13+", 
                            "7+", "all"))
 
+# Country Map
 binary <- lapply(movies$Country, function(x) {
   vals <- unique(x) 
   x <- setNames(rep(1,length(vals)), vals);
@@ -21,8 +23,18 @@ result <- do.call(plyr::rbind.fill, binary)
 names(result) <- gsub(".", " ", names(result), fixed = TRUE)
 result <- tibble::rowid_to_column(result, "ID")
 
-
 countrymovies <- movies %>% full_join(result)
+
+cc <- countrymovies %>% select(-ID,-Title,-Year,-Age,-IMDb,
+                               -`Rotten Tomatoes`,-Directors,
+                               -Genres,-Country, -Language, -Runtime) %>%
+  gather(country, value, -Netflix,-Hulu,-`Prime Video`,-`Disney+`) %>%
+  filter(value == '1') %>%
+  select(-value) %>% group_by(country) %>% 
+  summarise(Netflix=sum(Netflix=='1'),
+            Hulu=sum(Hulu=='1'),
+            `Prime Video`=sum(`Prime Video`=='1'),
+            `Disney+`=sum(`Disney+`=='1'))
 
 #Rotten Tomatoes Score - Density Plot
 RT<- movies %>% select(Netflix, Hulu, `Prime Video`, `Disney+`, `Rotten Tomatoes`)
@@ -43,8 +55,28 @@ IMDB_ <- IMDB_ %>% summarise(Netflix= ifelse(Netflix=='1',IMDb,NA),
 IMDB_ <- melt.data.table(IMDB_) %>% rename(`Streaming Service`= variable,
                                     `IMDb Score`= value)
 
+#Genre Data
+movies<- movies %>% mutate(Genres = as.character(Genres)) %>% 
+  mutate(Genres= strsplit(Genres, split=","))
+
+binary <- lapply(movies$Genres, function(x) {
+  vals <- unique(x) 
+  x <- setNames(rep(1,length(vals)), vals);
+  do.call(data.frame, as.list(x))
+})
+genres_result <- do.call(plyr::rbind.fill, binary)
+names(genres_result) <- gsub(".", " ", names(genres_result), fixed = TRUE)
+genres_result <- tibble::rowid_to_column(genres_result, "ID")
+genremovies <- movies %>% full_join(genres_result)
+
+totals <- genremovies %>% 
+  summarise(Netflix = sum(Netflix=='1'),
+            Hulu=sum(Hulu=='1'),
+            `Prime Video` = sum(`Prime Video`=='1'),
+            `Disney+` =sum(`Disney+`=='1')) %>%
+  gather('Streaming Service', 'Total')
 
 # select variables
 choice <- colnames(movies[7:10])
-countries <- colnames(countrymovies[16:183])
+countries <- colnames(cc[2:5])
 genre <- colnames(genremovies[16:42])
